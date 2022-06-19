bedac7dfd0eaf1ca02a6482683d0f17edff8edc3
Arkiralor <prithoo11335@gmail.com>
2022-06-20 04:09:37 +0530

    added profile pictures.

--- userapp/urls.py
+++ userapp/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from userapp.apis import GetUserView, AddUserView, UserLoginView, UserLogoutView, \
-    UserGetView, GetAllProfilesAPI, GenerateUserLoginOTPAPI, UserValidateOTPAPI
+from userapp.apis import GetUserView, AddUserView, UserLoginView, UserLogoutView, UserGetView, \
+    GetAllProfilesAPI, GetUserProfileAPI, GetOwnUserProfileAPI, \
+    GenerateUserLoginOTPAPI, UserValidateOTPAPI
 
 urlpatterns = [
     path('login/', UserLoginView.as_view(), name='user_login'),
@@ -9,6 +10,8 @@ urlpatterns = [
     path('logout/', UserLogoutView.as_view(), name='User_logout'),
     path('all/', GetUserView.as_view(), name='all_users'),
     path('add/', AddUserView.as_view(), name='add_user'),
-    path('details/<str:slug>', UserGetView.as_view(), name='get_user'),
+    path('details/<slug:slug>', UserGetView.as_view(), name='get_user'),
     path('profile/all', GetAllProfilesAPI.as_view(), name='all_user_profiles'),
+    path('profile/edit', GetOwnUserProfileAPI.as_view(), name='own_user_profile'),
+    path('profile/<slug:slug>', GetUserProfileAPI.as_view(), name='get_user_profile'),
 ]
